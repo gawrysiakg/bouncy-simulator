e0a4f46b4d865d9a0eac5a74a89831ffd89c9b57
Grzegorz Gawrysiak <grzegorz.gawrysiak2@gmail.com>
2024-05-01 22:43:56 +0200

    main algo fixed

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -23,36 +23,19 @@ export class BoardComponent {
   public buttonText = '';
   public isGameStarted = false;
 
-  // handleTileClick(x: number, y: number) {
-  //   if (this.currentOperation === Operation.SET_BALL) {
-  //     this.BOARD[x][y] = '1';
-  //     this._bouncyService.setBallPosition(x, y);
-  //     this.isBallVisible = true;
-  //     this.currentOperation = Operation.SET_OBSTACLE;
-  //   } else if (this.currentOperation === Operation.SET_OBSTACLE) {
-  //     if (this.BOARD[x][y] === '0') {
-  //       this.BOARD[x][y] = 'Y';
-  //     } else if (this.BOARD[x][y] === 'Y') {
-  //       this.BOARD[x][y] = '0';
-  //     }
-  //     // this.currentOperation = Operation.PLAY;
-  //   } else if (this.currentOperation === Operation.RESTART) {
-  //     this.BOARD = this._bouncyService.getBoard();
-  //   }
-  // }
-
   handleTileClick(x: number, y: number) {
     if (this.currentOperation === Operation.SET_BALL) {
-      this._bouncyService.setBallPosition(x, y);
-      this.isBallVisible = true;
-      this.currentOperation = Operation.SET_OBSTACLE;
+      if (this.BOARD[x][y] === '0') {
+        this._bouncyService.setBallPosition(x, y);
+        this.isBallVisible = true;
+        this.currentOperation = Operation.SET_OBSTACLE;
+      }
     } else if (this.currentOperation === Operation.SET_OBSTACLE) {
       if (this.BOARD[x][y] === '0') {
         this.BOARD[x][y] = 'Y';
       } else if (this.BOARD[x][y] === 'Y') {
         this.BOARD[x][y] = '0';
       }
-      //this._bouncyService.setObstacle(x, y);
     } else if (this.currentOperation === Operation.RESTART) {
       this.BOARD = this._bouncyService.getBoard();
     }
@@ -68,18 +51,11 @@ export class BoardComponent {
     }
   }
 
-  // handleObstacleOKButton() {
-  //   if (!this._bouncyService.getBallPosition()) {
-  //     alert('Set ball position first.');
-  //   } else {
-  //     this.currentOperation = Operation.PLAY;
-  //     this.isGameStarted = true;
-  //     this._bouncyService.startGame();
-  //   }
-  // }
-
   handleStopButton() {
     this.isGameStarted = false;
     this._bouncyService.stopGame();
+    this.areObstaclesVisible = false;
+    this.currentOperation = Operation.SET_BALL;
+    this.BOARD = this._bouncyService.getBoard();
   }
 }
--- src/app/bouncy.service.ts
+++ src/app/bouncy.service.ts
@@ -11,8 +11,8 @@ export class BouncyService {
   public BOARD: any = MAIN_BOARD;
   private ballPosition: [number, number] | undefined;
   private subscription: Subscription | undefined;
-  private directionX: number = 1; // Kierunek ruchu wzdłuż osi X
-  private directionY: number = 1; // Kierunek ruchu wzdłuż osi Y
+  private directionX: number = 1;
+  private directionY: number = 1;
 
   public getBoard() {
     return this.BOARD;
@@ -30,55 +30,78 @@ export class BouncyService {
   private move() {
     if (this.ballPosition) {
       const [x, y] = this.ballPosition;
-
-      // Oblicz kolejną pozycję piłki
       const nextX = x + this.directionX;
       const nextY = y + this.directionY;
 
-      // Sprawdź, czy następna pozycja mieści się w granicach planszy
-      if (
-        nextX >= 0 &&
-        nextX < this.BOARD.length &&
-        nextY >= 0 &&
-        nextY < this.BOARD[0].length
-      ) {
-        // Sprawdź, czy następne pole nie jest polem 'X' lub 'Y'
-        if (this.BOARD[nextX][nextY] !== 'X') {
-          if (this.BOARD[nextX][nextY] === 'Y') {
-            // Odbicie od pola 'Y', zmień pole na '0'
-            this.BOARD[nextX][nextY] = '0';
-            // Odbicie piłki od ściany
-            this.directionX *= -1;
-            this.directionY *= -1;
-          } else {
-            // Aktualizuj pozycję piłki na planszy
-            this.BOARD[x][y] = '0'; // Wyczyść bieżącą pozycję
-            this.BOARD[nextX][nextY] = '1'; // Ustaw piłkę na nowej pozycji
-            this.ballPosition = [nextX, nextY]; // Zaktualizuj pozycję piłki
-          }
-        } else {
-          // Odbicie piłki od ściany
-          this.directionX *= -1;
-          this.directionY *= -1;
-        }
-
-        // Losowo zmień kierunek po odbiciu
-        if (Math.random() < 0.3) {
-          // Losowo wybierz, czy zmieniać kierunek w osi X czy Y
-          const changeX = Math.random() < 0.5;
-          if (changeX) {
-            // Zmień kierunek w osi X
-            this.directionX *= -1;
-          } else {
-            // Zmień kierunek w osi Y
-            this.directionY *= -1;
-          }
-        }
+      if (this.isNextPositionValid(nextX, nextY)) {
+        this.updateBallPosition(x, y, nextX, nextY);
       } else {
-        // Odbicie piłki od ściany
-        this.directionX *= -1;
-        this.directionY *= -1;
+        this.calculateNewBallDirection();
+        const [newX, newY] = [x + this.directionX, y + this.directionY];
+        if (this.isNextPositionValid(newX, newY)) {
+          this.updateBallPosition(x, y, newX, newY);
+        }
       }
+    }
+  }
+
+  private isNextPositionValid(x: number, y: number): boolean {
+    return (
+      x >= 0 &&
+      x < this.BOARD.length &&
+      y >= 0 &&
+      y < this.BOARD[0].length &&
+      this.BOARD[x][y] !== 'X'
+    );
+  }
+
+  private updateBallPosition(
+    x: number,
+    y: number,
+    nextX: number,
+    nextY: number
+  ) {
+    if (this.BOARD[nextX][nextY] === 'Y') {
+      this.directionX = -this.directionX;
+      this.directionY = -this.directionY;
+      this.BOARD[nextX][nextY] = '0';
+    } else {
+      this.BOARD[nextX][nextY] = '1';
+      this.BOARD[x][y] = '0';
+      this.ballPosition = [nextX, nextY];
+    }
+  }
+
+  private calculateNewBallDirection(): void {
+    const [x, y] = this.ballPosition! || [];
+    const possibleDirections = [
+      { dx: 1, dy: 0 },
+      { dx: -1, dy: 0 },
+      { dx: 0, dy: 1 },
+      { dx: 0, dy: -1 },
+      { dx: 1, dy: 1 },
+      { dx: 1, dy: -1 },
+      { dx: -1, dy: 1 },
+      { dx: -1, dy: -1 },
+    ].filter(({ dx, dy }) => {
+      const nx = x + dx;
+      const ny = y + dy;
+      return (
+        nx >= 0 &&
+        nx < this.BOARD.length &&
+        ny >= 0 &&
+        ny < this.BOARD[0].length &&
+        this.BOARD[nx][ny] !== 'X'
+      );
+    });
+
+    if (possibleDirections.length > 0) {
+      const { dx, dy } =
+        possibleDirections[
+          Math.floor(Math.random() * possibleDirections.length)
+        ];
+      this.directionX = dx;
+      this.directionY = dy;
     }
   }
 
@@ -95,6 +118,11 @@ export class BouncyService {
     if (this.subscription) {
       this.subscription.unsubscribe();
       this.subscription = undefined;
+      this.BOARD = MAIN_BOARD;
+      if (this.ballPosition) {
+        const [x, y] = this.ballPosition;
+        this.BOARD[x][y] = '0';
+      }
     }
   }
 }
